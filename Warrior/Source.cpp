54545910d99f440e3c97d74cdcfe577e18a072be
PieceOfEon <danilskripka1996@gmail.com>
2022-08-10 19:01:01 +0300

    End

--- Warrior/Source.cpp
+++ Warrior/Source.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <thread>
 #include <mutex>
+#include <fstream>
 
 using namespace std;
 static bool st=true;
@@ -15,46 +16,53 @@ public:
 	virtual void BasicCha() = 0;
 };
 
-
 class Bow:public Weapon
 {
 	float dmg;
 	float atckspd;
 	float HP;
 	string bow;
+	string Clas = "Archer";
 public:
 	virtual void BasicCha() override
 	{
-		int n;
+		char n;
 		cout << "Enter nymber:\n";
 		cout << "1 - Elven Bow: Damage = 511.3, AtcSpd = 2.1\n";
 		cout << "2 - Draconic Bow: Damage = 581.4, AtcSpd = 2.7\n";
 		cout << "3 - Winter Bow: Damage = 481.8, AtcSpd = 1.7\n";
-		cin >> n;
-		if (n == 1)
+		re2:n= _getch();
+		if (n == '1')
 		{
 			bow = "Elven Bow";
 			dmg = 511.3;
 			atckspd = 2.1;
-			
 		}
-		if (n == 2)
+		else if (n == '2')
 		{
 			bow = "Draconic Bow";
 			dmg = 581.4;
-			atckspd = 2.7;
+			atckspd = 2.3;
 		}
-		if (n == 3)
+		else if (n == '3')
 		{
 			bow = "Winter Bow";
 			dmg = 481.8;
 			atckspd = 1.7;
+		}
+		else 
+		{
+			goto re2;
 		}
 		HP = 5600.53;
 		cout <<"Your Weapon: " << bow << "\nDamage = " << dmg << "\tAtcSpd = " << atckspd << "\tHP = "<<HP<<"\n";
 		
 	}
 
+	string getClas()
+	{
+		return Clas;
+	}
 	string getName()
 	{
 		return bow;
@@ -67,15 +75,9 @@ public:
 	{
 		return atckspd;
 	}
-
 	float getHP()
 	{
 		return HP;
-	}
-
-	void Print()
-	{
-		cout << bow << "\n" << dmg<<"\n";
 	}
 };
 
@@ -89,34 +91,37 @@ class WarriorWeapon :public Weapon
 public:
 	virtual void BasicCha() override
 	{
-		int n;
+		char n;
 		cout << "Enter nymber:\n";
 		cout << "1 - SamuraiLongSword: Damage = 250.2, AtcSpd = 1.3\n";
 		cout << "2 - AncientBlade: Damage = 277.3, AtcSpd = 1.34\n";
 		cout << "3 - DarkSword: Damage = 294, AtcSpd = 1.47\n";
-		cin >> n;
-		if (n == 1)
+		re3:n=_getch();
+		if (n == '1')
 		{
 			warriorWeapon = "SamuraiLongSword";
 			dmg = 250.2;
 			atckspd = 1.3;
 		}
-		if (n == 2)
+		else if (n == '2')
 		{
 			warriorWeapon = "AncientBlade";
 			dmg = 277.3;
 			atckspd = 1.34;
 		}
-		if (n == 3)
+		else if (n == '3')
 		{
 			warriorWeapon = "DarkSword";
 			dmg = 294;
 			atckspd = 1.47;
 		}
+		else 
+		{
+			goto re3;
+		}
+		
 		HP = 9300;
 		cout << "Your Weapon: " << warriorWeapon << "\nDamage = " << dmg << "\tAtcSpd = " << atckspd << "\tHP = "<<HP<<"\n";
-
-
 	}
 	
 	string getClas()
@@ -140,11 +145,6 @@ public:
 	{
 		return HP;
 	}
-
-	void Print()
-	{
-		cout << warriorWeapon << "\n" << dmg << "\n";
-	}
 };
 
 class MageWeapon :public Weapon
@@ -153,37 +153,46 @@ class MageWeapon :public Weapon
 	float dmg;
 	float atckspd;
 	string mageWeapon;
+	string Clas = "Mage";
 public:
 	virtual void BasicCha() override
 	{
-		int n;
+		char n;
 		cout << "Enter nymber:\n";
-		cout << "1 - ArcanaMace: Damage = 180, AtcSpd = 1.2\n";
-		cout << "2 - StaffOfWizardy: Damage = 160, AtcSpd = 1\n";
-		cout << "3 - MagicBlunt: Damage = 195, AtcSpd = 1.46\n";
-		cin >> n;
-		if (n == 1)
+		cout << "1 - ArcanaMace: Damage = 250.2, AtcSpd = 1\n";
+		cout << "2 - StaffOfWizardy: Damage = 277.3, AtcSpd = 1.1\n";
+		cout << "3 - MagicBlunt: Damage = 294, AtcSpd = 1.15\n";
+		re4:n=_getch();
+		if (n == '1')
 		{
 			mageWeapon = "ArcanaMace";
 			dmg = 250.2;
-			atckspd = 1.3;
+			atckspd = 1;
 		}
-		if (n == 2)
+		else if (n == '2')
 		{
 			mageWeapon = "StaffOfWizardy";
 			dmg = 277.3;
-			atckspd = 1.34;
+			atckspd = 1.1;
 		}
-		if (n == 3)
+		else if (n == '3')
 		{
 			mageWeapon = "MagicBlunt";
 			dmg = 294;
-			atckspd = 1.47;
+			atckspd = 1.15;
+		}
+		else 
+		{
+			goto re4;
 		}
 		HP = 4800;
 		cout << "Your Weapon: " << mageWeapon << "\nDamage = " << dmg << "\tAtcSpd = " << atckspd << "\tHP = "<<HP<<"\n";
 	}
 
+	string getClas()
+	{
+		return Clas;
+	}
 	string getName()
 	{
 		return mageWeapon;
@@ -196,15 +205,9 @@ public:
 	{
 		return atckspd;
 	}
-
 	float getHP()
 	{
 		return HP;
-	}
-
-	void Print()
-	{
-		cout << mageWeapon << "\n" << dmg << "\n";
 	}
 };
 
@@ -223,7 +226,6 @@ public:
 	{
 		this->Clas = Clas;
 	}
-
 	void setName(string name)
 	{
 		this->name = name;
@@ -252,12 +254,10 @@ public:
 	{
 		return atckspd;
 	}
-
 	float getHP()
 	{
 		return HP;
 	}
-
 	string getClas()
 	{
 		return Clas;
@@ -275,7 +275,6 @@ public:
 			cout <<"ATS: " << all[dodik].getATS()<<"\n";
 			cout <<"DMG: " << all[dodik].getDMG() << "\n";
 			cout<<"HP: "<<all[dodik].getHP()<<"\n";
-
 		}
 	}
 
@@ -284,27 +283,39 @@ public:
 		
 		if (all.size() > 1)
 		{
+			char P;
 			ALLiN(all);
 			cout << "Vibor 2 dodika: \n";
 			int d1,d2;
-			cout << "D1:";
-			cin >> d1;
-			cout << "D2:";
-			cin >> d2;
-			d1--;
-			d2--;
+		re6:cout << "D1:";
+			cin >> P;
+			if (isdigit(P) != 0 && P<=all.size()+48 && P>48)
+			{
+				d1 = P-49;
+			}
+			else 
+			{
+				goto re6;
+			}
+			re7:cout << "D2:";
+			cin >> P;
+			if (isdigit(P) != 0 || P <= all.size()+48 && P>48)
+			{
+				d2 = P - 49;
+			}
+			else
+			{
+				goto re7;
+			}
 			Player alo;
-			
+	
 			thread D1(&Player::Fight,alo, all, d1, d2);
-
-			this_thread::sleep_for(chrono::milliseconds(15));
+			/*this_thread::sleep_for(chrono::milliseconds(15));*/
 			thread D2(&Player::Fight,alo, all, d2, d1);
-			/*Fight(all, d2, d1);*/
 			
 			D1.join();
 			D2.join();
 		}
-
 	}
 
 	void Fight(vector<Player> all,int d1,int d2)
@@ -313,17 +324,14 @@ public:
 		srand(time(0));
 		st = true;
 		float const dm = all[d1].getDMG();
-		/*cout<<all[d1].Clas<<"\n";*/
-		
-		while (all[d1].getHP() >= 0 && all[d2].getHP() >= 0 && st == true)
+		while (all[d1].getHP() >= 0 && all[d2].getHP() >=0 && st == true)
 		{
-			
+			ofstream outfile("Fight.txt", ios::app);
 			if (all[d1].Clas == "Warrior")
 			{
 				int r;
 				
 				r = rand() % 10 + 1;
-				/*cout <<"\nR: " << r;*/
 				if (r == 1 || r == 2 || r == 3)
 				{
 					all[d1].setDMG(all[d1].getDMG() * 2.2);
@@ -332,109 +340,140 @@ public:
 				{
 					all[d1].setDMG(dm);
 				}
+			}
+			if (all[d1].Clas == "Mage")
+			{
+				int r;
 
+				r = rand() % 10 + 1;
+				if (r == 1 || r == 2)
+				{
+					all[d1].setDMG(all[d1].getDMG() * 4);
+				}
+				else
+				{
+					all[d1].setDMG(dm);
+				}
+			}
+			if (all[d1].Clas == "Archer")
+			{
+				int r;
+				r = rand() % 10 + 1;
+				if (r == 1 || r == 2 || r == 3 || r == 4|| r == 5)
+				{
+					all[d1].setDMG(all[d1].getDMG() * 3);
+				}
+				else
+				{
+					all[d1].setDMG(dm);
+				}
 			}
 			this_thread::sleep_for(chrono::milliseconds(int(all[d1].getATS()) * 1000));
-			mu.lock();
-			cout << all[d1].getName() << "<<-- did " << all[d1].getDMG() << " DMG -->> " << all[d2].getName() << "\n";
-			mu.unlock();
-			all[d2].setHP(all[d2].getHP() - all[d1].getDMG());
-			if (all[d2].getHP() > 0)
+			if (all[d1].getDMG() > dm)
 			{
-				mu.lock();
-				cout << all[d2].getName() << " Left HP: " << all[d2].getHP() << "\n";
-				mu.unlock();
+				if (all[d1].getHP() >= 0 && all[d2].getHP() >= 0 && st == true)
+				{
+					mu.lock();
+					cout << all[d1].getName() << "<<-- did Critical Hit! " << all[d1].getDMG() << " DMG -->> " << all[d2].getName() << "\n";
+					outfile << all[d1].getName() << "<<-- did Critical Hit! " << all[d1].getDMG() << " DMG -->> " << all[d2].getName() << "\n";
+					mu.unlock();
+					all[d2].setHP(all[d2].getHP() - all[d1].getDMG());
+				}
 			}
 			else
 			{
-				st = false;
-				all[d2].setHP(0);
-				mu.lock();
-				cout << all[d2].getName() << " Left HP: " << all[d2].getHP() << "\n";
-				mu.unlock();
-				break;
+				if (all[d1].getHP() >= 0 && all[d2].getHP() >= 0 && st == true)
+				{
+					mu.lock();
+					cout << all[d1].getName() << "<<-- did " << all[d1].getDMG() << " DMG -->> " << all[d2].getName() << "\n";
+					outfile << all[d1].getName() << "<<-- did " << all[d1].getDMG() << " DMG -->> " << all[d2].getName() << "\n";
+					mu.unlock();
+					all[d2].setHP(all[d2].getHP() - all[d1].getDMG());
+				}
 			}
+			if (all[d2].getHP() > 0)
+			{
+				if (all[d1].getHP() >= 0 && all[d2].getHP() >= 0 && st == true)
+				{
+					mu.lock();
+					cout << all[d2].getName() << " Left HP: " << all[d2].getHP() << "\n";
+					outfile << all[d2].getName() << " Left HP: " << all[d2].getHP() << "\n";
+					mu.unlock();
+				}
+			}
+			if(all[d2].getHP() <= 0)
+			{
+				if (st == true)
+				{
+					st = false;
+					all[d2].setHP(0);
+					mu.lock();
+					cout << all[d2].getName() << " Left HP: " << all[d2].getHP() << "\n";
+					outfile << all[d2].getName() << " Left HP: " << all[d2].getHP() << "\n";
+					mu.unlock();
+					cout <<"\n\t~)@(~You are Won!~)@(~ " << all[d1].getName()<<"\n";
+					outfile << "\n\t~)@(~You are Won!~)@(~ " << all[d1].getName()<<"\n";
+				}
+				return;
+			}outfile.close();
+			all[d1].setDMG(dm);
 			cout << "\n";
 		}
 	}
-	
 };
-
 
 int main()
 {
-	
 	srand(time(0));
 	vector<Bow> play;
 	vector<WarriorWeapon> play2;
 	vector	<MageWeapon> play3;
 	vector<Player> all;
-	
+	int kol = 0;
 	Bow bow2;
 	WarriorWeapon Warrior;
 	MageWeapon Mage;
-	int kol = 0;
 	char vvod;
 	do
 	{
 		system("cls");
 		cout << "1 - Class\n";
 		cout << "2 - Fight\n";
+		cout << "Esc - Exit\n";
 		vvod = _getch();
 		switch (vvod)
 		{
 		case'1':
 		{
-			int clas;
+			char clas;
 			cout << "Select class:\n";
 			cout << "1 - Archer\n";
 			cout << "2 - Warrior\n";
 			cout << "3 - Mage\n";
-			cin >> clas;
-			if (clas == 1)
+			cout << "Exit - Esc\n";
+			re1:clas = _getch();
+			if (clas == '1')
 			{
-				
-				
 				bow2.BasicCha();
 				play.push_back(bow2);
-				cout << "Print:\n";
-				play[play.size()-1].Print();
+				system("pause");
 				Player tmp;
 				tmp.setATS(bow2.getATS());
 				tmp.setDMG(bow2.getDMG());
 				tmp.setHP(bow2.getHP());
+				tmp.setClas(bow2.getClas());
 				string ima;
 				cout << "Enter name etomy dodiku\n";
 				cin >> ima;
 				tmp.setName(ima);
-				
 				all.push_back(tmp);
-				cout << "Print:\n";
-				for (int i = 0; i < all.size(); i++)
-				{
-					all[i].Print();
-					cout << "\n";
-				}
-				system("pause");
-				cout << "Print:\n";
-				for (int i = 0; i < play.size(); i++)
-				{
-					play[i].Print();
-					cout << "\n";
-					
-				}
-				
 				system("pause");
 				break;
 			}
-			if (clas == 2)
+			if (clas == '2')
 			{
-				
-				
 				Warrior.BasicCha();
 				play2.push_back(Warrior);
-				cout << "Print:\n";
-				play2[play2.size() - 1].Print();
 				system("pause");
 				Player tmp;
 				tmp.setATS(Warrior.getATS());
@@ -445,44 +484,46 @@ int main()
 				cout << "Enter name etomy dodiku\n";
 				cin >> ima;
 				tmp.setName(ima);
-
 				all.push_back(tmp);
-				cout << "Print:\n";
-				for (int i = 0; i < all.size(); i++)
-				{
-					all[i].Print();
-					cout << "\n";
-				}
-				
 				system("pause");
 				break;
-				
 			}
-			if (clas == 3)
+			if (clas == '3')
 			{
-				
 				Mage.BasicCha();
 				play3.push_back(Mage);
-				cout << "Print:\n";
-				play3[play3.size() - 1].Print();
 				system("pause");
-
-				cout << "Print:\n";
-				for (int i = 0; i < play3.size(); i++)
-				{
-					play3[i].Print();
-					cout << "\n";
-				}
+				Player tmp;
+				tmp.setATS(Mage.getATS());
+				tmp.setDMG(Mage.getDMG());
+				tmp.setHP(Mage.getHP());
+				tmp.setClas(Mage.getClas());
+				string ima;
+				cout << "Enter name etomy dodiku\n";
+				cin >> ima;
+				tmp.setName(ima);
+				all.push_back(tmp);
 				system("pause");
 				break;
 			}
+			if (clas == 27) 
+			{
+				break;
+			}
+			goto re1;
 		}
 		case'2':
 		{
 			if (all.size() >= 2)
 			{
+				kol++;
+				ofstream outfile("Fight.txt", ios::app);
+				outfile << "\t\tFight: " << kol<<"\n";
+				outfile.close();
 				all[0].Vibor(all);
-				
+				/*ofstream outfile("Fight.txt", ios::app);*/
+				outfile << "\n\n";
+				outfile.close();
 				system("pause");
 			}
 			else
